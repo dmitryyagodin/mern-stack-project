7e12b8350d9ef09a8d1e0386bdc5bea93198ea18
Dmitry Yagodin <dmitry.yagodin@gmail.com>
2021-02-15 14:09:04 +0200

    Passing data using children

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Learning MERN full-stack tools
 Based on the exercizes from 'Pro MERN stack' book by Vasan Subramanian
+
+In development:
+
+Added 2 package.json scripts lines:
+```json
+"start": "nodemon -w server server/server.js",
+"watch": "babel src --out-dir public --watch --verbose",
+```
+Run them in separate terminals to watch for changes in the server and public folders to recompile jsx into js and restart the server accordingly.
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,62 @@
-class HelloWorld extends React.Component {
+class IssueFilter extends React.Component {
   render() {
-    const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe', 'Ukraine'];
-    const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-    const message = helloContinents.join(' ');
+    return <div>Placeholder for IssueFilter</div>
+  };
+}
 
+class IssueRow extends React.Component {
+  render() {
+    const style = this.props.rowStyle;
     return (
-      <div title="Outer div">
-        <h1>{message}</h1>
-      </div>
+      <tr>
+        <td style={style}>{this.props.issue_id}</td>
+        <td style={style}>{this.props.issue_title}</td>
+      </tr>
     );
   }
 }
 
-const element = <HelloWorld />;
+class IssueTable extends React.Component {
+  render() {
+    const rowStyle = {border: "1px solid silver", padding: 4};
+    return (
+      <table style={{borderCollapse: "collapse"}}>
+        <thead>
+          <tr>
+            <th style={rowStyle}>ID</th>
+            <th style={rowStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <IssueRow rowStyle={rowStyle} issue_id={1} 
+             issue_title="Error in console when clicking Add"/>
+          <IssueRow rowStyle={rowStyle} issue_id={2}
+             issue_title="Missing bottom border on panel"/>
+        </tbody>
+      </table>
+    );
+  }
+}
 
-ReactDOM.render(element, document.getElementById('root'));
+class IssueAdd extends React.Component {
+  render() {
+    return <div>Placeholder for IssueAdded</div>
+  };
+}
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>IssueTracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </React.Fragment>
+    );
+  };
+}
+
+ReactDOM.render(<IssueList />, document.getElementById('root'));
